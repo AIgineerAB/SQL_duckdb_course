1e1d53ebfdbd01a0edad5ae1b559cf5fda68e3e6
kokchun <kokchungiang@gmail.com>
2024-10-24 14:08:03 +0200

    comments

--- explorations/ingest_hemnet_csv.sql
+++ explorations/ingest_hemnet_csv.sql
@@ -1,3 +1,4 @@
+-- ingest data from csv
 CREATE TABLE
     IF NOT EXISTS hemnet AS (
         SELECT
@@ -6,17 +7,67 @@ CREATE TABLE
             read_csv_auto ('data/hemnet_data_clean.csv')
     );
 
--- block 2
+-- describe
 DESC;
 
---block 3
+-- select star
 SELECT
     *
 FROM
     hemnet;
 
+-- count and alias 
 SELECT
-    COUNT(*)
+    COUNT(*) as total_rows
 FROM
     hemnet;
+
+SELECT
+    *
+FROM
+    hemnet
+LIMIT
+    1;
+
+DESC TABLE hemnet;
+
+-- information_schema
+SELECT
+    column_name,
+    data_type,
+    is_nullable
+FROM
+    information_schema.columns
+WHERE
+    table_name = 'hemnet';
+
+-- CTE 
+WITH
+    price_difference AS (
+        SELECT
+            address,
+            commune,
+            rooms,
+            final_price - asked_price as price_diff
+        FROM
+            hemnet
+    )
+SELECT
+    COUNT(
+        CASE
+            WHEN price_diff > 0 THEN 1
+        END
+    ) AS number_positive,
+    COUNT(
+        CASE
+            WHEN price_diff < 0 THEN 1
+        END
+    ) AS number_negative,
+    COUNT(
+        CASE
+            WHEN price_diff = 0 THEN 1
+        END
+    ) AS number_zero
+FROM
+    price_difference;
 
