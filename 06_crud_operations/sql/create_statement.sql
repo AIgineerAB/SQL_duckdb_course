7fb6c19cd1109a5b26e38862f84babae1b038158
kokchun <kokchungiang@gmail.com>
2024-11-03 15:02:29 +0100

    crud operations

--- 06_crud_operations/sql/create_statement.sql
+++ 06_crud_operations/sql/create_statement.sql
@@ -11,30 +11,27 @@ FROM
 
 
 -- sequences to get auto incrementing rows 
-CREATE SEQUENCE id_integer_sequence START 1;
+CREATE SEQUENCE IF NOT EXISTS id_sql_sequence START 1;
+
+CREATE SEQUENCE IF NOT EXISTS id_python_sequence START 1;
+
+CREATE SEQUENCE IF NOT EXISTS id_duckdb_sequence START 1;
 
 
 SELECT * FROM pg_catalog.pg_sequences;
 
 -- tables for each glossary table 
 CREATE TABLE IF NOT EXISTS database.sql (
-	id INTEGER DEFAULT nextval('id_integer_sequence'),
+	id INTEGER DEFAULT nextval('id_sql_sequence'),
 	word STRING,
 	description STRING);
 
 CREATE TABLE IF NOT EXISTS database.duckdb (
-	id INTEGER DEFAULT nextval('id_integer_sequence'),
+	id INTEGER DEFAULT nextval('id_duckdb_sequence'),
 	word STRING,
 	description STRING);
 
 CREATE TABLE IF NOT EXISTS programming.python (
-	id INTEGER DEFAULT nextval('id_integer_sequence'),
+	id INTEGER DEFAULT nextval('id_python_sequence'),
 	word STRING,
 	description STRING);
-
-
-
-
-
-
-
