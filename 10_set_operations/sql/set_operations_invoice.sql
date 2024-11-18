1dd70c3631070e1896636cc3c89e7cf343e25405
kokchun <kokchungiang@gmail.com>
2024-11-18 20:17:31 +0100

    set operations invoice

--- 10_set_operations/sql/set_operations_invoice.sql
+++ 10_set_operations/sql/set_operations_invoice.sql
@@ -5,8 +5,9 @@ FROM
 	staging.invoice_202408
 
 
+-- total number of rows in 202408 and 202410 combined removing duplicates 
 SELECT
-	COUNT(*)
+	COUNT(*) AS combined_number_rows 
 FROM
 	(
 	SELECT
@@ -18,3 +19,36 @@ UNION
 		*
 	FROM
 		staging.invoice_202410) AS combined_invoices;
+		
+-- combines all rows in 202408 and 202410 and removing duplicates
+SELECT
+	*
+FROM
+	(
+	SELECT
+		*
+	FROM
+		staging.invoice_202408
+UNION
+	SELECT
+		*
+	FROM
+		staging.invoice_202410) AS combined_invoices;
+	
+	
+
+SELECT
+	COUNT(*) AS number_common_supplier
+FROM
+	(
+	SELECT
+		Leverantör
+	FROM
+		staging.invoice_202408
+INTERSECT
+	SELECT
+		Leverantör
+	FROM
+		staging.invoice_202410) AS combined_invoices;
+		
+	
