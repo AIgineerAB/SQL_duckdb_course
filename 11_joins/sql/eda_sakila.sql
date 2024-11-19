210822bbc6d52c9521198d61a526a7bbf11d1d2e
kokchun <kokchungiang@gmail.com>
2024-11-19 09:18:36 +0100

    EDA init

---
+++ 11_joins/sql/eda_sakila.sql
@@ -0,0 +1,8 @@
+desc;
+
+CALL sqlite_attach('/Users/aigineer/Documents/teaching repos/sql_duckdb_course/11_joins/data/sqlite-sakila.db');
+
+SELECT * FROM main.film;
+
+SELECT * FROM main.film_actor;
+
